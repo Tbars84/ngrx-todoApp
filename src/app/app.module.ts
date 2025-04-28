cd178d3fa94f35e919ec7cff553015d491916ed6
Tbars84 <camoilo.tabares.84@gmail.com>
2025-04-28 17:37:19 -0500

    Creacion jerarquia de folders de components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,22 @@ import { appTODOReducers } from './app-reducers';
 
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
+import { LoginComponent } from './ingreso-egreso-app/components/auth/login/login.component';
+import { RegisterComponent } from './ingreso-egreso-app/components/auth/register/register.component';
+import { DashboardComponent } from './ingreso-egreso-app/components/dashboard/dashboard.component';
+import { IngresosEgresosComponent } from './ingreso-egreso-app/components/ingresos-egresos/ingresos-egresos.component';
+import { EstadisticasComponent } from './ingreso-egreso-app/components/ingresos-egresos/estadisticas/estadisticas.component';
+import { DetalleComponent } from './ingreso-egreso-app/components/ingresos-egresos/detalle/detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresosEgresosComponent,
+    EstadisticasComponent,
+    DetalleComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingreso-egreso-app/components/ingresos-egresos/ingresos-egresos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresos-egresos',
+  templateUrl: './ingresos-egresos.component.html',
+  styleUrl: './ingresos-egresos.component.css'
+})
+export class IngresosEgresosComponent {
+
+}
